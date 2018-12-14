b62633f75bef54e7332a18e4c013ae1e9ddceecd
Oliver-Finlay-CS <o.finlay@share.epsb.ca>
2018-12-14 11:32:32 -0700

    Initial commit

---
+++ asfqegaerh.java
@@ -0,0 +1,12 @@
+public class asfqegaerh
+{
+  public static void main (String[] args)
+  throws ArrayIndexOutOfBoundsException
+{
+  int i=0;
+  while(args[i] == "")
+{
+  System.out.println("Enter two numbers");
+}
+}
+  }
