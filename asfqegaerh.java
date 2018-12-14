4df74ca18385e7b04a8d3ba6b7f177f597175864
Oliver-Finlay-CS <o.finlay@share.epsb.ca>
2018-12-14 11:45:27 -0700

    3456say

--- asfqegaerh.java
+++ asfqegaerh.java
@@ -1,12 +1,20 @@
+import java.io.*;
+
 public class asfqegaerh
 {
-  public static void main (String[] args)
-  throws ArrayIndexOutOfBoundsException
-{
-  int i=0;
-  while(args[i] == "")
-{
-  System.out.println("Enter two numbers");
-}
-}
+
+  public int addNum(int numA, int numB)
+  {
+     return numA + numB;
   }
+
+
+  public static void main (String args[])
+  throws IOException
+  {
+    AddNum obj = new AddNum();
+    int sum = obj.addNum(10, 20);
+
+  System.out.println("\nSum of 10 and 20 is: " + sum);
+}
+}
